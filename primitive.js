9c5e2f3f1db34530cdc39328859b8113854c618f
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-09-09 09:14:36 -0500

    Create primitive.js

---
+++ primitive.js
@@ -0,0 +1,12 @@
+function prim(){
+ var test = "DECOUVRE";
+ return test.valueOf();
+}
+prim();
+// String
+// Number
+// Null
+// Undefined
+// Symbol
+// Boolean
+// not an object, does not have method, not mutable valueOf() returns the primitive object value encapsulated.
